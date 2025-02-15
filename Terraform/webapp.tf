905d0c5fff2d90d3d5c7d5a000a6dd36432a5e53
Achimek <joachim.rachuta@gmail.com>
2025-02-15 20:20:48 +0100

    add

--- Terraform/webapp.tf
+++ Terraform/webapp.tf
@@ -12,7 +12,7 @@ resource "azurerm_linux_web_app" "app" {
   service_plan_id     = azurerm_service_plan.serviceplan.id
 
   identity {
-    type = "systemAssigned"
+    type = "SystemAssigned"
   }
 
   site_config {}
