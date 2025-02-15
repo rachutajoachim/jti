5b804b3fa5919a7e6f453780a1f5a056fb70d1ce
Achimek <joachim.rachuta@gmail.com>
2025-02-15 20:22:15 +0100

    add

--- Terraform/acr.tf
+++ Terraform/acr.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_registry" "acr" {
-  name                = "jti-${var.environment}-acr"
+  name                = "jti${var.environment}acr"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
