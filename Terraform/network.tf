562cc6e5c81b2691dd66baf82c9a55fd30defcbd
Achimek <joachim.rachuta@gmail.com>
2025-02-16 10:03:40 +0100

    a

--- Terraform/network.tf
+++ Terraform/network.tf
@@ -35,7 +35,7 @@ resource "azurerm_subnet" "subnetwebapp" {
     }
   }
 }
-resource "azurerm_subnet" "subnet_pe" {
+resource "azurerm_subnet" "subnetpe" {
   name                 = "mysql-private-endpoint-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
