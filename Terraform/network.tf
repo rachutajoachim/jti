d958aa2c5311f877cb8a0a669cf060d69403d8a6
Achimek <joachim.rachuta@gmail.com>
2025-02-16 09:11:44 +0100

    a

--- Terraform/network.tf
+++ Terraform/network.tf
@@ -10,16 +10,8 @@ resource "azurerm_subnet" "subnetsql" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.0.0/29"]
-  delegation {
-    name = "fs"
-    service_delegation {
-      name = "Microsoft.DBforMySQL/flexibleServers"
-      actions = [
-       "Microsoft.Network/virtualNetworks/subnets/action"
-      ]
-    }
-  }
 }
+
 resource "azurerm_subnet" "subnetwebapp" {
   name                 = "default2"
   resource_group_name  = azurerm_resource_group.rg.name
