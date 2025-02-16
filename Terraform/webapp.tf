5804e397b2a7fa3436f3d3b9c161857a5145b528
Achimek <joachim.rachuta@gmail.com>
2025-02-16 20:21:24 +0100

    a

--- Terraform/webapp.tf
+++ Terraform/webapp.tf
@@ -17,7 +17,7 @@ resource "azurerm_linux_web_app" "app" {
 
   site_config {}
   app_settings = {
-    "DB_HOST"                         = azurerm_mysql_flexible_server.sql.name
+    "DB_HOST"                         = "${azurerm_mysql_flexible_server.sql.name}.mysql.database.azure.com"
     "DB_USER"                         = "achim"
     "DB_PASS"                         = data.azurerm_key_vault_secret.sqlpassword.value
     "DB_NAME"                         = "jti"
