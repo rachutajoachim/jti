d4677afb61ee8bce47986379d304a8b682c96cc0
Achimek <joachim.rachuta@gmail.com>
2025-02-26 21:09:23 +0100

    a

--- Terraform/webapp.tf
+++ Terraform/webapp.tf
@@ -3,7 +3,8 @@ resource "azurerm_service_plan" "serviceplan" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
-  sku_name            = "B1"
+  sku_name            = "P0v3"
+
 }
 resource "azurerm_linux_web_app" "app" {
   name                = "jti-${var.environment}-app"
