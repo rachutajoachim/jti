acd927cf5ea6445f616fc5fbe8628cd411a82802
Achimek <joachim.rachuta@gmail.com>
2025-02-26 21:28:21 +0100

    a

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -83,12 +83,11 @@ resource "azurerm_mysql_flexible_database" "example" {
   collation           = "utf8_unicode_ci"
 }
 
-resource "azurerm_dns_a_record" "example" {
+resource "azurerm_private_dns_a_record" "example" {
   name                = "jit${var.environment}sql"
   zone_name           = "privatelink.mysql.database.azure.com"
   resource_group_name = azurerm_resource_group.rg.name
   ttl                 = 10
   records             = [azurerm_private_endpoint.mysql_private_endpoint.private_service_connection[0].private_ip_address]
 
-      depends_on = [azurerm_private_dns_zone.dns, azurerm_private_endpoint.mysql_private_endpoint, azurerm_private_dns_zone_virtual_network_link.example]
 }
